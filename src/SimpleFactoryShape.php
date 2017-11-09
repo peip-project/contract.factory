b4d42780b6a0d58790df8fcb80f81946923b81e4
Timo Michna <timomichna@yahoo.de>
2017-11-09 13:35:21 +0100

    add SimpleFactoryShape

---
+++ src/SimpleFactoryShape.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the PEIP contract.factory package.
+ *  (c) 2017 Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PEIP\Contract\Factory;
+
+/**
+ * Class PEIP\Contract\Factory\SimpleFactoryShape.
+ *
+ */
+interface SimpleFactoryShape
+{
+    /**
+     * @param array ...$args
+     * @return object
+     */
+    public function build(...$args);
+}
