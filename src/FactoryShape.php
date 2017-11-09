b424e51a218ee8dd8bc8ad25916e0512a0d61b91
Timo Michna <timomichna@yahoo.de>
2017-11-09 13:35:56 +0100

    add FactoryShape

---
+++ src/FactoryShape.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the semtic package.
+ *  (c) ${THIS_YEAR} Timo Michna <timomichna/yahoo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PEIP\Contract\Factory;
+
+/**
+ * Interface PEIP\Contract\Factory\FactoryShape.
+ *
+ */
+interface FactoryShape extends SimpleFactoryShape
+{
+}
